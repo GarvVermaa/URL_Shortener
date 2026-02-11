80f807275113e87c753198c1085562e4e7042453
Garv Verma <GarvVermaa@users.noreply.github.com>
2026-02-11 23:35:14 +0530

    Created Secure Signup route for user

---
+++ routes/user.routes.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import { db } from '../db/index.js'
+import { usersTable } from '../models/user.model.js';
+import { randomBytes, createHmac } from 'node:crypto';
+
+const router = express.Router();
+
+router.post('/signup', async (req, res) => {
+  const { firstname, lastname, email, password } = req.body();
+
+  //!Can be used but very manual and basic
+  // if (!firstname) {
+  //   return res.status(400).json({ error: `Firstname is required` });
+  // }  
+
+  const [existingUser] = await db
+    .select({ id: usersTable.id, })
+    .from(usersTable)
+    .where(eq(email, usersTable.email));
+
+  if (existingUser) {
+    return res.status(400).json({ error: `User with email: ${email} already exists` });
+
+    const salt = randomBytes(256).toString('hex');
+    const hashedPassword = createHmac('sha256', salt).update(password).digest('hex')
+
+    const [user] = await db.insert(usersTable).values({
+      firstname,
+      lastname,
+      email,
+      salt,
+      password: hashedPassword
+    }).returning({ id: usersTable.id });
+
+    return res.status(201).json({ data: { userId: user.id } })
+  }
+
+});
+
+export default router;
