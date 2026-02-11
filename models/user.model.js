9f64e2dc2c4dac8039c809d4338ad2bf3d502a6e
Garv Verma <GarvVermaa@users.noreply.github.com>
2026-02-11 23:16:47 +0530

    implemented User Schema using Drizzle ORM

--- models/user.model.js
+++ models/user.model.js
@@ -1,11 +1,13 @@
-import {pgTable,uuid,varchar } from 'drizzle-orm/pg-core';
+import { timestamp } from 'drizzle-orm/gel-core';
+import { pgTable, uuid, varchar, text, timestamp } from 'drizzle-orm/pg-core';
 
-export const usersTable= pgTable("users",{
+export const usersTable = pgTable("users", {
   id: uuid().primaryKey().defaultRandom(),
-  firstname: varchar('first_name',{lenght: 25}).notNull(),
-  lastname: varchar('last_name',{lenght: 25}),
-  email: varchar({lenght:255}).notNull().unique(),
+  firstname: varchar('first_name', { lenght: 25 }).notNull(),
+  lastname: varchar('last_name', { lenght: 25 }),
+  email: varchar({ lenght: 255 }).notNull().unique(),
   password: text().notNull(),
-  salt: text().notNull
-
+  salt: text().notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').$onUpdate(() => new Date()),
 });
